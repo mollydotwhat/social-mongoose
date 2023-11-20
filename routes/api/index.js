f5973c0211d1cacb6274eb78bc54453919ec143b
molly <molly.salzberger@gmail.com>
2023-11-20 08:14:09 -0600

    api routes skeletoning

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,10 @@
 const router = require('express').Router();
 
 const thoughtRoutes = require('./thought.routes');
+const userRoutes = require('./user.routes');
+const reactionRoutes = require('./reaction.routes');
+
+
+
 
 module.exports = router;
