987ef04c01fa5aef560c8b553685d107e461b288
molly <molly.salzberger@gmail.com>
2023-11-20 07:43:01 -0600

    modifying boilerplate

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // boilerplate we walked through in class
 // MODIFY where useful
 const express = require('express');
-// Run npm install mongodb and require mongodb and MongoClient class
-const { MongoClient } = require('mongodb');
+// connecting to database/mongoose
+const db = require('./config/connection');
 
 const app = express();
 const port = 3001;
@@ -13,11 +13,10 @@ const connectionStringURI = `mongodb://127.0.0.1:27017`;
 // Initialize a new instance of MongoClient
 const client = new MongoClient(connectionStringURI);
 
-// Declare a variable to hold the connection
-let db;
+
 
 // Create variable to hold our database name
-const dbName = 'shelterDB';
+const dbName = 'socialDB';
 
 // Use connect method to connect to the mongo server
 client.connect()
@@ -38,24 +37,24 @@ client.connect()
 // Built in Express function that parses incoming requests to JSON
 app.use(express.json());
 
-app.post('/create', (req, res) => {
-  // Use db connection to add a document
-  db.collection('petCollection').insertOne(
-    { name: req.body.name, breed: req.body.breed }
-  )
-    .then(results => res.json(results))
-    .catch(err => {
-      if (err) throw err;
-    });
-});
+// app.post('/create', (req, res) => {
+//   // Use db connection to add a document
+//   db.collection('petCollection').insertOne(
+//     { name: req.body.name, breed: req.body.breed }
+//   )
+//     .then(results => res.json(results))
+//     .catch(err => {
+//       if (err) throw err;
+//     });
+// });
 
-app.get('/read', (req, res) => {
-  // Use db connection to find all documents in collection
-  db.collection('petCollection')
-    .find()
-    .toArray()
-    .then(results => res.json(results))
-    .catch(err => {
-      if (err) throw err;
-    });
-});
+// app.get('/read', (req, res) => {
+//   // Use db connection to find all documents in collection
+//   db.collection('petCollection')
+//     .find()
+//     .toArray()
+//     .then(results => res.json(results))
+//     .catch(err => {
+//       if (err) throw err;
+//     });
+// });
