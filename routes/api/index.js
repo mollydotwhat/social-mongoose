1627f7fa67840e10b2f90a7799902ddf554c3ce9
molly <molly.salzberger@gmail.com>
2023-11-20 08:16:56 -0600

    more routes and note to self

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,7 +4,8 @@ const thoughtRoutes = require('./thought.routes');
 const userRoutes = require('./user.routes');
 const reactionRoutes = require('./reaction.routes');
 
-
+// haven't decided path structure yet
+// router.use('/path', route)
 
 
 module.exports = router;
