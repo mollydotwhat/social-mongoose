1d295dd3c3a25dcf10957ad768c48f56055fb625
molly <molly.salzberger@gmail.com>
2023-12-13 08:43:15 -0600

    more controller skeleton, remembered about email validator in user model

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -7,13 +7,56 @@ module.exports = {
     // get all thoughts
     async allThoughts (req, res) {
         try {
-            // const payload = await 
+            const payload = await Thought.find().populate({ path: 'userId' }).select('__v')
+            res.json({status: 'success', payload})
 
+        } catch (err) {
+            res.status(500).json({status: 'error', payload: err.message});
+        }
+    },
+
+    // get one by ids
+    async oneThought (req, res) {
+        try {
+            // const payload = await 
+    
+            // res.json({status: 'success', payload})
+        } catch (err) {
+            res.status(500).json({status: 'error', payload: err.message});
+        }
+    },
+
+    // create one
+    async newThought (req, res) {
+        try {
+            // const payload = await 
+            // remember to push thought id to user's thoughts array
+            // res.json({status: 'success', payload})
+        } catch (err) {
+            res.status(500).json({status: 'error', payload: err.message});
+        }
+    },
+
+    // edit one
+    async updateThought (req, res) {
+        try {
+            // const payload = await 
+    
+            // res.json({status: 'success', payload})
+        } catch (err) {
+            res.status(500).json({status: 'error', payload: err.message});
+        }
+    },
+
+    // remove one
+    async deleteThought (req, res) {
+        try {
+            // const payload = await 
+    
             // res.json({status: 'success', payload})
         } catch (err) {
             res.status(500).json({status: 'error', payload: err.message});
         }
     }
-
 
 }
--- controllers/userController.js
+++ controllers/userController.js
@@ -12,6 +12,50 @@ async allUsers (req, res) {
     } catch (err) {
         res.status(500).json({status: 'error', payload: err.message});
     }
+},
+
+// one by id
+async oneUser (req, res) {
+    try {
+        // const payload = await 
+
+        // res.json({status: 'success', payload})
+    } catch (err) {
+        res.status(500).json({status: 'error', payload: err.message});
+    }
+},
+
+// new user
+async createUser (req, res) {
+    try {
+        // const payload = await 
+
+        // res.json({status: 'success', payload})
+    } catch (err) {
+        res.status(500).json({status: 'error', payload: err.message});
+    }
+},
+
+// update user info
+async changeUser (req, res) {
+    try {
+        // const payload = await 
+
+        // res.json({status: 'success', payload})
+    } catch (err) {
+        res.status(500).json({status: 'error', payload: err.message});
+    }
+}, 
+
+// remove user
+async deleteUser (req, res) {
+    try {
+        // const payload = await 
+
+        // res.json({status: 'success', payload})
+    } catch (err) {
+        res.status(500).json({status: 'error', payload: err.message});
+    }
 }
 
 
