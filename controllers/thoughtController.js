0efc44356965469fde1fa096ebed2a969e7915c8
molly <molly.salzberger@gmail.com>
2023-12-13 08:47:43 -0600

    all thought controller function names

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -3,60 +3,81 @@ const { Thought } = require('../models');
 
 
 module.exports = {
-    
+
     // get all thoughts
-    async allThoughts (req, res) {
+    async allThoughts(req, res) {
         try {
             const payload = await Thought.find().populate({ path: 'userId' }).select('__v')
-            res.json({status: 'success', payload})
+            res.json({ status: 'success', payload })
 
         } catch (err) {
-            res.status(500).json({status: 'error', payload: err.message});
+            res.status(500).json({ status: 'error', payload: err.message });
         }
     },
 
     // get one by ids
-    async oneThought (req, res) {
+    async oneThought(req, res) {
         try {
             // const payload = await 
-    
+
             // res.json({status: 'success', payload})
         } catch (err) {
-            res.status(500).json({status: 'error', payload: err.message});
+            res.status(500).json({ status: 'error', payload: err.message });
         }
     },
 
     // create one
-    async newThought (req, res) {
+    async newThought(req, res) {
         try {
             // const payload = await 
             // remember to push thought id to user's thoughts array
             // res.json({status: 'success', payload})
         } catch (err) {
-            res.status(500).json({status: 'error', payload: err.message});
+            res.status(500).json({ status: 'error', payload: err.message });
         }
     },
 
     // edit one
-    async updateThought (req, res) {
+    async updateThought(req, res) {
         try {
             // const payload = await 
-    
+
             // res.json({status: 'success', payload})
         } catch (err) {
-            res.status(500).json({status: 'error', payload: err.message});
+            res.status(500).json({ status: 'error', payload: err.message });
         }
     },
 
     // remove one
-    async deleteThought (req, res) {
+    async deleteThought(req, res) {
         try {
             // const payload = await 
-    
+
             // res.json({status: 'success', payload})
         } catch (err) {
-            res.status(500).json({status: 'error', payload: err.message});
+            res.status(500).json({ status: 'error', payload: err.message });
         }
-    }
+    },
 
+    // create one
+    async newReaction(req, res) {
+        try {
+            // const payload = await 
+            // remember to push reaction to thought's reaction array? check this syntax
+            // res.json({status: 'success', payload})
+        } catch (err) {
+            res.status(500).json({ status: 'error', payload: err.message });
+        }
+    },
+
+    async deleteReaction(req, res) {
+        try {
+            // const payload = await 
+
+            // res.json({status: 'success', payload})
+        } catch (err) {
+            res.status(500).json({ status: 'error', payload: err.message });
+        }
+    },
+    
 }
